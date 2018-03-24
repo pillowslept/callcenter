5090d357203ba62c87bae651e83c37d56351d8c8
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-03-24 17:45:29 -0500

    Se ajustan pruebas con constantes.

--- src/test/java/com/almundo/callcenter/DispatcherTest.java
+++ src/test/java/com/almundo/callcenter/DispatcherTest.java
@@ -3,6 +3,7 @@ package com.almundo.callcenter;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +20,22 @@ public class DispatcherTest {
 	private static final int AVAILABLE_EMPLOYEES = 10;
 	private static final int SLEEP_TEN_SECONDS = 10000;
 	private static final int SLEEP_TWENTY_SECONDS = 20000;
+	private static final int FIVE_CALLS = 5;
+	private static final int TEN_CALLS = 10;
+	private static final int FIFTEEN_CALLS = 15;
+	private static final int ZERO = 0;
+	private static final int ONE = 1;
 
+	@Before
+	public void init(){
+		dispatcher.clean();
+	}
+	
 	@Test
 	public void dispatchTenCallsTest() {
-		int incomingCalls = 10;
+		int incomingCalls = TEN_CALLS;
 
-		for (int callNumber = 1; callNumber <= incomingCalls; callNumber++) {
+		for (int callNumber = ONE; callNumber <= incomingCalls; callNumber++) {
 			sleep(100);
 			dispatcher.dispatchCall(createCall(callNumber));
 		}
@@ -34,17 +45,17 @@ public class DispatcherTest {
 		List<IncomingCall> callsAtended = dispatcher.getCallsAtended();
 		List<IncomingCall> callsAtendedAfterBusy = dispatcher.getCallsAtendedAfterBusy();
 		List<Employee> employees = dispatcher.getAvailableEmployees();
-
+		System.out.println(callsAtended.size());
 		Assert.assertTrue(callsAtended.size() == incomingCalls);
-		Assert.assertTrue(callsAtendedAfterBusy.size() == 0);
+		Assert.assertTrue(callsAtendedAfterBusy.size() == ZERO);
 		assertAllEmployeesFree(employees);
 	}
 	
 	@Test
 	public void dispatchFifteenCallsTest() {
-		int incomingCalls = 15;
+		int incomingCalls = FIFTEEN_CALLS;
 
-		for (int callNumber = 1; callNumber <= incomingCalls; callNumber++) {
+		for (int callNumber = ONE; callNumber <= incomingCalls; callNumber++) {
 			sleep(100);
 			dispatcher.dispatchCall(createCall(callNumber));
 		}
@@ -56,15 +67,15 @@ public class DispatcherTest {
 		List<Employee> employees = dispatcher.getAvailableEmployees();
 
 		Assert.assertTrue(callsAtended.size() == incomingCalls);
-		Assert.assertTrue(callsAtendedAfterBusy.size() == incomingCalls - 10);
+		Assert.assertTrue(callsAtendedAfterBusy.size() == incomingCalls - TEN_CALLS);
 		assertAllEmployeesFree(employees);
 	}
 	
 	@Test
 	public void dispatchFiveCallsTest() {
-		int incomingCalls = 5;
+		int incomingCalls = FIVE_CALLS;
 
-		for (int callNumber = 1; callNumber <= incomingCalls; callNumber++) {
+		for (int callNumber = ONE; callNumber <= incomingCalls; callNumber++) {
 			sleep(100);
 			dispatcher.dispatchCall(createCall(callNumber));
 		}
@@ -76,7 +87,7 @@ public class DispatcherTest {
 		List<Employee> employees = dispatcher.getAvailableEmployees();
 
 		Assert.assertTrue(callsAtended.size() == incomingCalls);
-		Assert.assertTrue(callsAtendedAfterBusy.size() == 0);
+		Assert.assertTrue(callsAtendedAfterBusy.size() == ZERO);
 		assertAllEmployeesFree(employees);
 	}
 
