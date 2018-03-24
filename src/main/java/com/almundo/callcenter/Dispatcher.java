c0503ece0649334ad1c502695f27277b23fd288f
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-03-24 11:23:44 -0500

    Se modifica despachador de clases.

--- src/main/java/com/almundo/callcenter/Dispatcher.java
+++ src/main/java/com/almundo/callcenter/Dispatcher.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +13,11 @@ public class Dispatcher {
 	
 	private static final Logger LOGGER = Logger.getLogger(Dispatcher.class.getName());
 
-	Employee operator;
-	Employee director;
-	Employee supervisor;
-
+	@Autowired
+	AvailableEmployees availableEmployees;
+	
 	List<IncomingCall> callsQueve = new ArrayList<>();
 	List<IncomingCall> callsAtended = new ArrayList<>();
-	
-	public Dispatcher(){
-		createAvailableEmployes();
-	}
 	
 	@Async	
 	public void dispatchCall(IncomingCall incomingCall){
@@ -29,30 +25,27 @@ public class Dispatcher {
 		try {
 			assignEmployee(incomingCall);
 		} catch (InterruptedException e) {
-			LOGGER.error("Ocurrió un error atendiendo llamada de " + incomingCall.getPersonName());	
+			LOGGER.error("Ocurrió un error atendiendo llamada de " + incomingCall.getCallNumber());	
 		}
 	}
 
 	private void assignEmployee(IncomingCall incomingCall) throws InterruptedException {
-		if(operator.isAvailable()){
-			attend(incomingCall, operator);
-		}else if(supervisor.isAvailable()){
-			attend(incomingCall, supervisor);
-		}else if(director.isAvailable()){
-			attend(incomingCall, director);
+		Employee employee = availableEmployees.assignEmployee();
+		if(employee != null){
+			attend(incomingCall, employee);
 		}else{
+			LOGGER.info("No hay empleado disponible que atienda llamada : " + incomingCall.getCallNumber() + ", se pondrá en espera, muchas gracias.");
 			callsQueve.add(incomingCall);
 		}
 	}
 	
 	private void attend(IncomingCall incomingCall, Employee employee) throws InterruptedException{
-		employee.setAvailable(Boolean.FALSE);
 		int callDuration = CallConfiguration.getDuration();
 		Thread.sleep(callDuration);
 		LOGGER.info("Se atendió llamada: " + incomingCall.getCallNumber()
-				+ ", empleado asignado: " + employee.getPosition()
+				+ ", empleado asignado: " + employee.getEmployeePosition()
 				+ ", duración llamada: " + callDuration + " segundos");
-		employee.setAvailable(Boolean.TRUE);
+		availableEmployees.freeEmployee(employee);
 		callsAtended.add(incomingCall);
 		validateCallsQueve();
 	}
@@ -66,12 +59,9 @@ public class Dispatcher {
 	public List<IncomingCall> getCallsAtended() {
 		return callsAtended;
 	}
-
-	private void createAvailableEmployes(){
-		LOGGER.info("Creando empleados disponibles");
-		operator = new Employee("Juan", "Operador");
-		director = new Employee("Pedro", "Director");
-		supervisor = new Employee("Ana", "Supervisor");
+	
+	public List<Employee> getAvailableEmployees(){
+		return availableEmployees.getAvailableEmployees();
 	}
-
+	
 }
--- src/test/java/com/almundo/callcenter/DispatcherTest.java
+++ src/test/java/com/almundo/callcenter/DispatcherTest.java
@@ -1,5 +1,6 @@
 package com.almundo.callcenter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.*;
@@ -20,18 +21,42 @@ public class DispatcherTest {
 		int incomingCalls = 10;
 
 		for (int callNumber = 1; callNumber <= incomingCalls; callNumber++) {
-			IncomingCall incomingCall = new IncomingCall();
-			incomingCall.setCallNumber(callNumber);
-			dispatcher.dispatchCall(incomingCall);
+			dispatcher.dispatchCall(createCall(callNumber));
 		}
 		
-		sleep(50000);
+		sleep(10000);
 		
 		List<IncomingCall> callsAtended = dispatcher.getCallsAtended();
+		List<Employee> employees = dispatcher.getAvailableEmployees();
 
 		Assert.assertTrue(callsAtended.size() == 10);
+		Assert.assertTrue(employees.size() == 10);
+		Assert.assertTrue(employees.get(0).isAvailable());
+		Assert.assertTrue(employees.get(1).isAvailable());
+		Assert.assertTrue(employees.get(2).isAvailable());
+		Assert.assertTrue(employees.get(3).isAvailable());
+		Assert.assertTrue(employees.get(4).isAvailable());
+		Assert.assertTrue(employees.get(5).isAvailable());
+		Assert.assertTrue(employees.get(6).isAvailable());
+		Assert.assertTrue(employees.get(7).isAvailable());
+		Assert.assertTrue(employees.get(8).isAvailable());
+		Assert.assertTrue(employees.get(9).isAvailable());
 	}
 	
+	private IncomingCall createCall(int callNumber){
+		IncomingCall incomingCall = new IncomingCall();
+		incomingCall.setCallNumber(callNumber);
+		return incomingCall;
+	}
+	
+	private List<IncomingCall> createCalls(int callsQuantity){
+		List<IncomingCall> incomingCalls = new ArrayList<>();
+		for (int callNumber = 1; callNumber <= callsQuantity; callNumber++) {
+			incomingCalls.add(createCall(callNumber));
+		}
+		return incomingCalls;
+	}
+		
 	private void sleep(int time) {
 		try {
 			Thread.sleep(time);
